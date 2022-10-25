ac4134b4d6946c97bd081ed7a5693a29c0bd989a
tony <anthonyherrera24@gmail.com>
2022-10-24 19:50:55 -0500

    Added first commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# sd_batch_job
-My wife won't let me buy a GPU, so I'll pay for one one hour at a time
+# Stable Diffusion Batch Jobs
+
+> My wife won't let me buy a GPU, so I'll pay for one one hour at a time
+
+The idea behind is that I should be able to upload parameters, have a message sent off to a lambda
+and then have message sent to a batch job with a GPU to do my bidding. Easy enough, right?
+
+Infrastructure looks a bit like this - very simple. Just upload an S3 file, get picked up by a Lambda
+and then launched into a batch environment.
+
+![image](docs/infrastructure.drawio.png)
+
+
---
+++ src/main.py
@@ -0,0 +1,48 @@
+"""
+Main file that runs the stable diffusion pipeline
+Initial implementation comes from here: https://huggingface.co/blog/stable_diffusion
+"""
+
+from diffusers import StableDiffusionPipeline
+
+def get_secret() -> str:
+    """
+    Retrieves the token for the hugging FACE model
+    :return:
+    """
+
+    secret_name = "huggingface"
+    region_name = "us-east-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    secret_value = client.get_secret_value(
+        SecretId=secret_name
+    )
+    return secret_value
+
+
+def main():
+    """
+    Runs the mains code - inference and saving the image
+    :return:
+    """
+
+    #TODO get token
+
+    pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", use_auth_token=TOKEN)
+    pipe.to("cuda")
+
+    prompt = "a photograph of an astronaut riding a horse"
+
+    image = pipe(prompt)["sample"][0]
+
+    image.save(f"astronaut_rides_horse.png")
+
+if __name__ == "__main__":
+    print("Hello")
